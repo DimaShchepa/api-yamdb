3a4931cd3858aae7331b04f82768cce1bb43b899
Дмитрий Щепа <shchepa2000@mail.ru>
2026-06-01 14:36:16 +0700

    Models done

--- api_yamdb/creations/models.py
+++ api_yamdb/creations/models.py
@@ -1,37 +1,85 @@
 from django.db import models
+from django.utils import timezone
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import AbstractUser
 
 
-class Categories(models.Model):
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('user', 'Пользователь'),
+        ('moderator', 'Модератор'),
+        ('admin', 'Администратор'),
+    ]
+    email = models.EmailField(unique=True, max_length=254)
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150, null=True, blank=True)
+    last_name = models.CharField(max_length=150, null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    role = models.CharField(choices=ROLE_CHOICES, default='user')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
+
+
+class Category(models.Model):
     name = models.CharField(max_length=256)
     slug = models.SlugField(unique=True, max_length=50)
 
+    def __str__(self):
+        return self.name
 
-class Genres(models.Model):
+
+class Genre(models.Model):
     name = models.CharField(max_length=256,)
     slug = models.SlugField(unique=True, max_length=50)
 
+    def __str__(self):
+        return self.name
 
-class Titles(models.Model):
-    category = models.ForeignKey(Categories,
+
+class Title(models.Model):
+    category = models.ForeignKey(Category,
                                  on_delete=models.SET_NULL,
                                  null=True,
                                  blank=True,
                                  related_name='titles')
-    genre = models.ForeignKey(Genres,
+    genre = models.ForeignKey(Genre,
                               on_delete=models.SET_NULL,
                               null=True,
                               blank=True,
                               related_name='titles')
     name = models.CharField(max_length=256,)
-    year = models.IntegerField()
-    description = models.TextField(max_length=256)
-    rating = models.IntegerField(null=True, blank=True)
+    year = models.IntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(timezone.now().year)
+        ]
+    )
+    description = models.TextField()
+    rating = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
-class Reviews(models.Model):
+class Review(models.Model):
     text = models.CharField(max_length=256)
     score = models.IntegerField(validators=[MinValueValidator(1),
                                             MaxValueValidator(10)])
-    title = models.ForeignKey(Titles, on_delete=models.CASCADE,
+    title = models.ForeignKey(Title, on_delete=models.CASCADE,
                               related_name='reviews')
+    pub_date = models.DateTimeField(auto_now_add=True, db_index=True)
+
+
+class Comment(models.Model):
+    title = models.ForeignKey(Title, on_delete=models.CASCADE,
+                              related_name='comments')
+    review = models.ForeignKey(Review, on_delete=models.CASCADE,
+                               related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='comments')
+    text = models.CharField()
+    pub_date = models.DateTimeField(auto_now_add=True, db_index=True)
